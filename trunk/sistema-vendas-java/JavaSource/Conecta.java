3acca048c456fdca1d022bb06c0e7607d5768ec9
fellipedf@gmail.com <fellipedf@gmail.com@1727be4e-1b17-6a25-02cb-720713510281>
2013-04-29 14:54:18 +0000

    Conexão ao banco com hibernat

--- trunk/sistema-vendas-java/JavaSource/Conecta.java
+++ trunk/sistema-vendas-java/JavaSource/Conecta.java
@@ -2,16 +2,15 @@ import org.hibernate.Session;
 
 import com.fellipe.sistema.vendas.HimernateUtil;
 
-
 public class Conecta {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Session sessao = null;
-		try{
+		try {
 			sessao = HimernateUtil.getSession().openSession();
 			System.out.println("Conectou !");
-			
-		}finally{
+
+		} finally {
 			sessao.close();
 			System.out.println("Fechou conexão");
 		}
--- trunk/sistema-vendas-java/JavaSource/com/fellipe/sistema/vendas/HimernateUtil.java
+++ trunk/sistema-vendas-java/JavaSource/com/fellipe/sistema/vendas/HimernateUtil.java
@@ -4,24 +4,23 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HimernateUtil {
-	
+
 	private static final SessionFactory session = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
-		try{
+		try {
 			Configuration cfg = new Configuration();
 			cfg.configure("hibernate.cfg.xml");
 			return cfg.buildSessionFactory();
-		}catch (Throwable e){
+		} catch (Throwable e) {
 			System.out.println("Deu pau! Manolo!\n " + e);
 			throw new ExceptionInInitializerError();
 		}
 	}
-		public static SessionFactory getSession(){
-			return session;
-		
-		
+
+	public static SessionFactory getSession() {
+		return session;
+
 	}
-	
 
 }
